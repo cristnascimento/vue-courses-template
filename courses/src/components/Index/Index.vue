7002a24b15bf6f8ca3e19fa276627deec09df76b
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-24 13:34:48 -0300

    Adding about area 1 component. closes #15

--- courses/src/components/Index/Index.vue
+++ courses/src/components/Index/Index.vue
@@ -3,61 +3,7 @@
         <slider-area></slider-area>
         <services-area></services-area>
         <courses-area></courses-area>
-        <!--? About Area-1 Start -->
-        <section class="about-area1 fix pt-10">
-            <div class="support-wrapper align-items-center">
-                <div class="left-content1">
-                    <div class="about-icon">
-                        <img src="/assets/img/icon/about.svg" alt="">
-                    </div>
-                    <!-- section tittle -->
-                    <div class="section-tittle section-tittle2 mb-55">
-                        <div class="front-text">
-                            <h2 class="">Learn new skills online with top educators</h2>
-                            <p>The automated process all your website tasks. Discover tools and 
-                                techniques to engage effectively with vulnerable children and young 
-                            people.</p>
-                        </div>
-                    </div>
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Techniques to engage effectively with vulnerable children and young people.</p>
-                        </div>
-                    </div>
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Join millions of people from around the world  learning together.</p>
-                        </div>
-                    </div>
-
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Join millions of people from around the world learning together. Online learning is as easy and natural.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="right-content1">
-                    <!-- img -->
-                    <div class="right-img">
-                        <img src="/assets/img/gallery/about.png" alt="">
-
-                        <div class="video-icon" >
-                            <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0"><i class="fas fa-play"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- About Area End -->
+        <about-area1></about-area1>
         <!--? top subjects Area Start -->
         <div class="topic-area section-padding40">
             <div class="container">
@@ -314,6 +260,7 @@
 import SliderArea from './SliderArea'
 import ServicesArea from './ServicesArea'
 import CoursesArea from './CoursesArea'
+import AboutArea1 from './AboutArea1'
 
 export default {
     data () {
@@ -324,7 +271,8 @@ export default {
     components: {
         "slider-area": SliderArea,
         "services-area": ServicesArea,
-        "courses-area": CoursesArea
+        "courses-area": CoursesArea,
+        "about-area1": AboutArea1
     }
 }
 </script>
