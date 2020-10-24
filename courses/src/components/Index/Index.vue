aceb4976af62ee9131d2727d095397f3237c1806
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-24 15:29:29 -0300

    Adding about area 3 component. closes #17

--- courses/src/components/Index/Index.vue
+++ courses/src/components/Index/Index.vue
@@ -5,52 +5,7 @@
         <courses-area></courses-area>
         <about-area1></about-area1>
         <top-subjects-area></top-subjects-area>
-        <!--? About Area-3 Start -->
-        <section class="about-area3 fix">
-            <div class="support-wrapper align-items-center">
-                <div class="right-content3">
-                    <!-- img -->
-                    <div class="right-img">
-                        <img src="/assets/img/gallery/about3.png" alt="">
-                    </div>
-                </div>
-                <div class="left-content3">
-                    <!-- section tittle -->
-                    <div class="section-tittle section-tittle2 mb-20">
-                        <div class="front-text">
-                            <h2 class="">Learner outcomes on courses you will take</h2>
-                        </div>
-                    </div>
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Techniques to engage effectively with vulnerable children and young people.</p>
-                        </div>
-                    </div>
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Join millions of people from around the world
-                            learning together.</p>
-                        </div>
-                    </div>
-                    <div class="single-features">
-                        <div class="features-icon">
-                            <img src="/assets/img/icon/right-icon.svg" alt="">
-                        </div>
-                        <div class="features-caption">
-                            <p>Join millions of people from around the world learning together.
-                            Online learning is as easy and natural.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- About Area End -->
+        <about-area3></about-area3>
         <!--? Team -->
         <section class="team-area section-padding40 fix">
             <div class="container">
@@ -145,6 +100,7 @@ import ServicesArea from './ServicesArea'
 import CoursesArea from './CoursesArea'
 import AboutArea1 from './AboutArea1'
 import TopSubjectsArea from './TopSubjectsArea'
+import AboutArea3 from './AboutArea3'
 
 export default {
     data () {
@@ -157,7 +113,8 @@ export default {
         "services-area": ServicesArea,
         "courses-area": CoursesArea,
         "about-area1": AboutArea1,
-        "top-subjects-area": TopSubjectsArea
+        "top-subjects-area": TopSubjectsArea,
+        "about-area3": AboutArea3
     }
 }
 </script>
