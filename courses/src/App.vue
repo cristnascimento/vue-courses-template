a2d8882722d8016b5497663589d84b98108ba0f7
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-24 11:46:21 -0300

    Adding layout component. closes #7

--- courses/src/App.vue
+++ courses/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view></router-view>
+  <layout>
+    <router-view></router-view>
+  </layout>
 </template>
 
 <script>
--- courses/src/main.js
+++ courses/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import Index from './components/Index'
+import Layout from './components/Layout'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,5 +17,6 @@ const app = createApp(App)
 app.use(router)
 
 app.component('my-index', Index)
+app.component('layout', Layout)
 
 app.mount('#app')
