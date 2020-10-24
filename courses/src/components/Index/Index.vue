4d9d7f989be433c6a30f6dc1e21d721773c8551b
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-24 15:31:37 -0300

    Adding team component. closes #18

--- courses/src/components/Index/Index.vue
+++ courses/src/components/Index/Index.vue
@@ -6,65 +6,7 @@
         <about-area1></about-area1>
         <top-subjects-area></top-subjects-area>
         <about-area3></about-area3>
-        <!--? Team -->
-        <section class="team-area section-padding40 fix">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-xl-7 col-lg-8">
-                        <div class="section-tittle text-center mb-55">
-                            <h2>Community experts</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="team-active">
-                    <div class="single-cat text-center">
-                        <div class="cat-icon">
-                            <img src="/assets/img/gallery/team1.png" alt="">
-                        </div>
-                        <div class="cat-cap">
-                            <h5><a href="services.html">Mr. Urela</a></h5>
-                            <p>The automated process all your website tasks.</p>
-                        </div>
-                    </div>
-                    <div class="single-cat text-center">
-                        <div class="cat-icon">
-                            <img src="/assets/img/gallery/team2.png" alt="">
-                        </div>
-                        <div class="cat-cap">
-                            <h5><a href="services.html">Mr. Uttom</a></h5>
-                            <p>The automated process all your website tasks.</p>
-                        </div>
-                    </div>
-                    <div class="single-cat text-center">
-                        <div class="cat-icon">
-                            <img src="/assets/img/gallery/team3.png" alt="">
-                        </div>
-                        <div class="cat-cap">
-                            <h5><a href="services.html">Mr. Shakil</a></h5>
-                            <p>The automated process all your website tasks.</p>
-                        </div>
-                    </div>
-                    <div class="single-cat text-center">
-                        <div class="cat-icon">
-                            <img src="/assets/img/gallery/team4.png" alt="">
-                        </div>
-                        <div class="cat-cap">
-                            <h5><a href="services.html">Mr. Arafat</a></h5>
-                            <p>The automated process all your website tasks.</p>
-                        </div>
-                    </div>
-                    <div class="single-cat text-center">
-                        <div class="cat-icon">
-                            <img src="/assets/img/gallery/team3.png" alt="">
-                        </div>
-                        <div class="cat-cap">
-                            <h5><a href="services.html">Mr. saiful</a></h5>
-                            <p>The automated process all your website tasks.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <team></team>
         <!-- Services End -->
         <!--? About Area-2 Start -->
         <section class="about-area2 fix pb-padding">
@@ -101,6 +43,7 @@ import CoursesArea from './CoursesArea'
 import AboutArea1 from './AboutArea1'
 import TopSubjectsArea from './TopSubjectsArea'
 import AboutArea3 from './AboutArea3'
+import Team from './Team'
 
 export default {
     data () {
@@ -114,7 +57,9 @@ export default {
         "courses-area": CoursesArea,
         "about-area1": AboutArea1,
         "top-subjects-area": TopSubjectsArea,
-        "about-area3": AboutArea3
+        "about-area3": AboutArea3,
+        "team": Team
+
     }
 }
 </script>
